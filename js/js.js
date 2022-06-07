c691e0329d066808d50a8976c59a1953cc632c61
Tomas Wienert <tomy.wienert@hotmail.com>
2022-06-07 14:10:38 -0300

    primera subida

---
+++ js/js.js
@@ -0,0 +1,13 @@
+const iva = 1.21 ;
+
+let cerrarCarrito = "";
+
+let productoSeleccionado = "" ;
+
+function precioProducto (productoSeleccionado,iva) {
+    let precio = productoSeleccionado * iva;
+}
+
+function sumaCarrito (cerrarCarrito,precio) {
+    
+}
