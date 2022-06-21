ab5609606ac903162bec6c428c4ac907eb9990b8
Tomas Wienert <tomy.wienert@hotmail.com>
2022-06-21 20:16:37 -0300

    se termina el html

--- js/js.js
+++ js/js.js
@@ -9,29 +9,54 @@ class producto {
         this.nombre = nombre
         this.precio = precio
     }
+    mostrarProducto() {
+
+    }
 }
 
-const platoPlayoTsuji = new producto("tsuji", "Plato playo", Math.round(1800 * iva * margenGanancia))
-const platoPlayoVerbano = new producto("verbano", "Plato playo", Math.round(1400 * iva * margenGanancia))
-const platoPlayoCorona = new producto("corona", "Plato playo", Math.round(800 * iva * margenGanancia))
-const platoPlayoBiona = new producto("biona", "Plato playo", Math.round(900 * iva * margenGanancia))
+const platoPlayoTsuji1 = new producto("tsuji", "Plato playo", Math.round(1870 * iva * margenGanancia))
+const platoPlayoTsuji2 = new producto("tsuji", "Plato playo", Math.round(1800 * iva * margenGanancia))
+const platoPlayoTsuji3 = new producto("tsuji", "Plato playo", Math.round(1760 * iva * margenGanancia))
+const platoPlayoVerbano1 = new producto("verbano", "Plato playo", Math.round(1450 * iva * margenGanancia))
+const platoPlayoVerbano2 = new producto("verbano", "Plato playo", Math.round(1370 * iva * margenGanancia))
+const platoPlayoVerbano3 = new producto("verbano", "Plato playo", Math.round(1430 * iva * margenGanancia))
 
-const platoOndoTsuji = new producto("tsuji", "Plato ondo", Math.round(1900 * iva * margenGanancia))
-const platoOndoVerbano = new producto("verbano", "Plato ondo", Math.round(1450 * iva * margenGanancia))
-const platoOndoCorona = new producto("corona", "Plato ondo", Math.round(850 * iva * margenGanancia))
-const platoOndoBiona = new producto("biona", "Plato ondo", Math.round(950 * iva * margenGanancia))
+const platoHondoTsuji1 = new producto("tsuji", "Plato hondo", Math.round(1890 * iva * margenGanancia))
+const platoHondoTsuji2 = new producto("tsuji", "Plato hondo", Math.round(1950 * iva * margenGanancia))
+const platoHondoTsuji3 = new producto("tsuji", "Plato hondo", Math.round(1870 * iva * margenGanancia))
+const platoHondoVerbano1 = new producto("verbano", "Plato hondo", Math.round(1650 * iva * margenGanancia))
+const platoHondoVerbano2 = new producto("verbano", "Plato hondo", Math.round(1600 * iva * margenGanancia))
+const platoHondoVerbano3 = new producto("verbano", "Plato hondo", Math.round(1530 * iva * margenGanancia))
 
-const platoPostreTsuji = new producto("tsuji", "Plato postre", Math.round(1500 * iva * margenGanancia))
-const platoPostreVerbano = new producto("verbano", "Plato postre", Math.round(1300 * iva * margenGanancia))
-const platoPostreCorona = new producto("corona", "Plato postre", Math.round(650 * iva * margenGanancia))
-const platoPostreBiona = new producto("biona", "Plato postre", Math.round(700 * iva * margenGanancia))
+const platoPostreTsuji = new producto("tsuji", "Plato postre", Math.round(1300 * iva * margenGanancia))
+const platoPostreVerbano = new producto("verbano", "Plato postre", Math.round(1150 * iva * margenGanancia))
 
-const platoTazaCafeTsuji = new producto("tsuji", "Plato postre", Math.round(1600 * iva * margenGanancia))
-const platoTazaCafeVerbano = new producto("verbano", "Plato postre", Math.round(1350 * iva * margenGanancia))
-const platoTazaCafeCorona = new producto("corona", "Plato postre", Math.round(700 * iva * margenGanancia))
-const platoTazaCafeBiona = new producto("biona", "Plato postre", Math.round(800 * iva * margenGanancia))
+const juegoCafeTsuji1 = new producto("tsuji", "Juego cafe", Math.round(2800 * iva * margenGanancia))
+const juegoCafeTsuji2 = new producto("tsuji", "Juego cafe", Math.round(2600 * iva * margenGanancia))
+const juegoCafeTsuji3 = new producto("tsuji", "Juego cafe", Math.round(2700 * iva * margenGanancia))
+const juegoCafeVerbano1 = new producto("verbano", "Juego cafe", Math.round(2350 * iva * margenGanancia))
+const juegoCafeVerbano2 = new producto("verbano", "Juego cafe", Math.round(2500 * iva * margenGanancia))
+const juegoCafeVerbano3 = new producto("verbano", "Juego cafe", Math.round(2420 * iva * margenGanancia))
 
-/* hago una clase para armar el carrito y que tenga adentro la funciones que necesito que cuumpla */
+let descuentoTsuji = new producto("tsuji", "Juego completo", 11050)
+let descuentoVerbano = new producto("tsuji", "Juego completo", 9556)
+
+/* La uso para obtener precios de los distintos productos creados */
+
+/* console.log(juegoCafeVerbano1.precio) */
+
+/* Creo una funcion para aceptar solo pedidos menores o iguales a 24 unidades */
+
+function pedidoCorrecto () {
+    if (cantidadIngresada < 0 && cantidadIngresada > 24) {
+        alert("Ingresó un valor incorrecto, por favor coloque entre 1 y 24 unidades")
+    }
+}
+
+
+/* hago una clase para armar el carrito y que tenga adentro la funciones que necesito que cumpla */
+
+/* let usuario = prompt("Ingrese su nombre") */
 
 class carrito {
     constructor() {
